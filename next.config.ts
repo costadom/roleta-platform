c576bf17eec3a692fd678e3f86604a79168b40dc
costadom <160781915+costadom@users.noreply.github.com>
2026-03-23 12:47:54 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* Caso tenha imagens configuradas ou outras coisas, mantenha. 
+     Apenas APAGUE o bloco que diz "eslint: { ... }" */
 };
 
 export default nextConfig;
